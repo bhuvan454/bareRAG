051beda037c379d9b8daf4c4470b4361f7de6ce0
bhuvan454 <bhuvankumarchennoju@gmail.com>
2024-05-30 00:04:27 -0500

    deleted dev notebook

--- backend/apps/rag/generate_embeddings.py
+++ backend/apps/rag/generate_embeddings.py
@@ -7,30 +7,8 @@ from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain_community.embeddings.bedrock import BedrockEmbeddings
 
 
+
 def get_embedding_function():
-    # embeddings = BedrockEmbeddings(
-    #     credentials_profile_name="default", region_name="us-east-1"
-    # )
     embeddings = OllamaEmbeddings()
     return embeddings
 
-# class EmbeddingsGenerator:
-#     def __init__(self, embeddings_type: str):
-#         if embeddings_type == 'ollama':
-#             self.embeddings = OllamaEmbeddings( model= "llama:7b")
-#         elif embeddings_type == 'bedrock':
-#             self.embeddings = BedrockEmbeddings(
-#                 # credentials=os.environ['BEDROCK_CREDENTIALS'],
-#                 # region_name=os.environ['BEDROCK_REGION'],
-#                 credentials_profile_name = 'default',
-#                 region_name = 'us-west-1'
-#             )
-#         else:
-#             raise ValueError(f'Unknown embeddings type: {embeddings_type}')
-
-
-
-#     def embedding_function(self):
-#         return self.embeddings 
-#     # def getembeddings(self, document: Document):
-#     #     return self.embeddings.embed_documents(document)
